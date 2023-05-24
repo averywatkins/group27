7d2a9f2e04f6c1368a8384d34c5ad2824bae4940
Shreya Ravi <sravi2@stanford.edu>
2023-05-23 20:28:13 -0700

    user reporting flow mostly done. insert links and perhaps fix formatting.

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -7,16 +7,47 @@ class State(Enum):
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
+    CONFIRM_MESSAGE = auto()
+    OBTAIN_REASON = auto()
+    OBTAIN_VICTIM = auto()
+    VICTIM_AGE = auto()
+    PERPETRATOR_AGE = auto()
+    HARASSMENT_TYPE = auto()
+    OTHER_HARASSMENT_TYPE = auto()
+    DETECT_CSAM = auto()
+    MORE_EVIDENCE = auto()
+
+class Category(Enum):
+    NULL = auto()
+    HARASSMENT = auto()
+    CONTENT = auto()
+
+class Victim(Enum):
+    NULL = auto()
+    ME = auto()
+    OTHER = auto()
+
+class PerpAge(Enum):
+    UNKNOWN = auto()
+    UNDER_18 = auto()
+    OVER_18 = auto()
 
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
+    harassment_types = ["Blackmail", "Threats of Physical Harm", "Solicitation", "Manipulation", "Other"]
 
     def __init__(self, client):
         self.state = State.REPORT_START
         self.client = client
         self.message = None
+        self.category = Category.NULL
+        self.victim = Victim.NULL
+        self.victim_age = -1
+        self.perp_age = PerpAge.UNKNOWN
+        self.harassment_type = []
+        self.more_evidence = None
     
     async def handle_message(self, message):
         '''
@@ -24,7 +55,7 @@ class Report:
         prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
         get you started and give you a model for working with Discord. 
         '''
-
+        print("report for message: ", message)
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
             return ["Report cancelled."]
@@ -54,14 +85,191 @@ class Report:
                 return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
 
             # Here we've found the message - it's up to you to decide what to do next!
-            self.state = State.MESSAGE_IDENTIFIED
+            self.state = State.CONFIRM_MESSAGE
             return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+                    "Is this the message you intended to report?"]
         
-        if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
 
+        # ask user if this is the message they intended to report -- wait for YES/NO
+        if self.state == State.CONFIRM_MESSAGE:
+            confirm_response = message.content.strip().lower()
+            if confirm_response == "yes":
+                self.state = State.MESSAGE_IDENTIFIED
+            elif confirm_response == "no":
+                self.state = State.AWAITING_MESSAGE
+                reply = "Please copy paste the link to the message you want to report.\n"
+                reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
+                return [reply]
+            else:
+                return ["Please indicate whether this is the messsage you intended to report with a `yes` or `no`."]
+            
+        # ask user for high level category 
+        if self.state == State.MESSAGE_IDENTIFIED:
+            self.state = State.OBTAIN_REASON
+            reply = self.prompt_options("What is the reason for reporting?", 
+                               ["Sexual content involving a minor",
+                                "Sexual harassment of a minor",
+                                "Other"])
+            return [reply]
+        
+        # record high level category 
+        if self.state == State.OBTAIN_REASON:
+            response = message.content.strip()
+            reply = self.prompt_options("Who is being targeted?", ["Me", "Someone else"])
+            if response == "1":
+                self.category = Category.CONTENT
+                self.state = State.OBTAIN_VICTIM
+                return [reply]
+            elif response == "2":
+                self.category = Category.HARASSMENT
+                self.state = State.OBTAIN_VICTIM
+                return [reply]
+            elif response == "3":  
+                self.state = State.REPORT_COMPLETE
+                return ["Thank you for submitting this report. Your report will be handled by other reporting flows."]
+            else:
+                reply = self.reprompt_options("What is the reason for reporting?", 
+                               ["Sexual content involving a minor",
+                                "Sexual harassment of a minor",
+                                "Other"])
+                return [reply]
+        
+        if self.state == State.OBTAIN_VICTIM:
+            response = message.content.strip()
+            if response == "1": # The reporter is the victim
+                self.victim = Victim.ME
+                self.state = State.VICTIM_AGE
+                reply = "How old are you?"
+                return [reply]
+            elif response == "2": # The victim is someone else
+                self.victim = Victim.OTHER
+                self.state = State.VICTIM_AGE
+                reply = "How old is the victim? If you don't know, type `idk`."
+                return [reply]
+            else:
+                reply = self.prompt_options("Who is being targeted?", ["Me", "Someone else"])
+                return [reply]
+            
+        if self.state == State.VICTIM_AGE:
+            response = message.content.strip().lower()
+            prompt = "How old is the person targeting " + ("you" if self.victim == Victim.ME else "the victim") + "?"
+            reply = self.prompt_options(prompt, ["Under 18", "Over 18", "I don't know"])
+            if response == "idk":
+                self.victim_age = -1
+                self.state = State.PERPETRATOR_AGE
+                return [reply]
+            try:
+                self.victim_age = int(response)
+                self.state = State.PERPETRATOR_AGE
+                return [reply]
+            except ValueError:
+                return ["Please enter the age as a number. If you don't know, type `idk`."]
+        
+        if self.state == State.PERPETRATOR_AGE:
+            response = message.content.strip().lower()
+            if response == "1":
+                self.perp_age = PerpAge.UNDER_18
+            elif response == "2":
+                self.perp_age = PerpAge.OVER_18
+            elif response == "3":
+                self.perp_age = PerpAge.UNKNOWN
+            else:
+                prompt = "How old is the person targeting " + ("you" if self.victim == Victim.ME else "the victim") + "?"
+                reply = self.reprompt_options(prompt, ["Under 18", "Over 18", "I don't know"])
+                return [reply]
+            self.state = State.HARASSMENT_TYPE
+            reply = self.prompt_options_choose_many("What type of harassment is this message?",
+                                ["Blackmail", "Threats of Physical Harm",
+                                 "Solicitation", "Manipulation", "Other"])
+            return [reply]
+
+        if self.state == State.HARASSMENT_TYPE:
+            response = message.content.strip().lower()
+            for i in range(1, 5):
+                if str(i) in response:
+                    self.harassment_type.append(self.harassment_types[i-1])
+            if "5" in response:
+                self.state = State.OTHER_HARASSMENT_TYPE
+                return ["You indicated `Other`. Please state the category the harassment you are reporting falls into."]
+            if len(self.harassment_type) == 0:
+                reply = self.reprompt_options_choose_many("What type of harassment is this message?",
+                                ["Blackmail", "Threats of Physical Harm",
+                                 "Solicitation", "Manipulation", "Other"])
+                return [reply]
+            
+            self.state = State.DETECT_CSAM
+            prompt = "Does the message include inappropriate images or videos? "
+            prompt += "Here’s some types of content that may be considered inappropriate."
+            reply = self.prompt_options(prompt, ["Yes", "No"])
+            return [reply]
+
+        if self.state == State.OTHER_HARASSMENT_TYPE:
+            response = message.content.strip().lower()
+            self.harassment_type.append(response)
+            self.state = State.DETECT_CSAM
+            prompt = "Does the message include inappropriate images or videos? "
+            prompt += "Here’s some types of content that may be considered inappropriate."
+            reply = self.prompt_options(prompt, ["Yes", "No"])
+            return [reply]
+        
+        if self.state == State.DETECT_CSAM:
+            response = message.content.strip().lower()
+            if response == "1": 
+                self.state = State.REPORT_COMPLETE
+                reply = "Thank you for reporting. Our content moderation team will review your report shortly. "
+                reply += "Here are some mental health resources for additional support: (insert a few links). "
+                reply += "Another way to get this image taken down **anonymously** is to report it to takeitdown.org."
+                return [reply]
+            elif response == "2": 
+                self.state = State.MORE_EVIDENCE
+                reply = "Would you like to upload further evidence? If so, please include it all in your next message. "
+                reply += "If not, type `no`."
+                return [reply]
+            else:
+                prompt = "Does the message include inappropriate images or videos? "
+                prompt += "Here’s some types of content that may be considered inappropriate."
+                reply = self.reprompt_options(prompt, ["Yes", "No"])
+                return [reply]
+            
+        if self.state == State.MORE_EVIDENCE:
+            response = message.content.strip().lower()
+            if response != "no":
+                # save the evidence for the manual review
+                self.more_evidence = response
+
+            self.state = State.REPORT_COMPLETE
+            reply = "Thank you for reporting. Our content moderation team will review the report shortly. Would you like to block this user?\n"
+            reply += "The user will **NOT** be notified of this action and any further attempts from this user to communicate with you will be blocked.\n"
+            reply += "Here is a link showing you how to block a user. (insert link here)"
+            return [reply]
+        
         return []
+    
+    def reprompt_options_choose_many(self, prompt, options):
+        reply = "Please choose at least one category.\n"
+        reply += prompt + "\n"
+        reply += "Please choose all types that apply. Include each number in the message (e.g. `1,2`).\n"
+        reply += "\n".join(["{}. {}".format(i+1, options[i]) for i in range(len(options))])
+        return reply
+    
+    def prompt_options_choose_many(self, prompt, options):
+        reply = prompt + "\n"
+        reply += "Please choose all types that apply. Include each number in the message (e.g. `1,2`).\n"
+        reply += "\n".join(["{}. {}".format(i+1, options[i]) for i in range(len(options))])
+        return reply
+    
+    def prompt_options(self, prompt, options):
+        reply = prompt + "\n"
+        reply += "Please reply with the corresponding number.\n"
+        reply += "\n".join( ["{}. {}".format(i+1, options[i]) for i in range(len(options))] )
+        return reply
+    
+    def reprompt_options(self, prompt, options):
+        reply = "Please choose a valid number from the given options.\n"
+        reply += prompt + "\n"
+        reply += "Please reply with the corresponding number.\n"
+        reply += "\n".join(["{}. {}".format(i+1, options[i]) for i in range(len(options))])
+        return reply
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
